1356b01b294e7d62fdbfbc44294b0480e1b60011
Ivan Solovev <ivan.solovev@qt.io>
2022-05-25 15:33:17 +0200

    Android: fix crash on exit when QGeoAreaMonitorPolling is used
    QGeoAreaMonitorPolling creates a Q_GLOBAL_STATIC instance of its
    QGeoAreaMonitorPollingPrivate (QGAMPP) to share the same backend among
    multiple instances.
    
    That is normally not an issue, but the QtPositioning Android backend
    uses another couple of Q_GLOBAL_STATIC QMaps to keep track of the
    created position or satellite info sources.
    
    QGAMPP is trying to access these static QMaps in its destructor (when
    destroying a child QGeoPositionInfoSource). But the maps may already
    be destroyed at the point when QGAMPP wants to access them.
    
    This patch fixes it by explicitly checking QGlobalStatic::exists()
    before accessing the maps.
    
    Fixes: QTBUG-103807
    Change-Id: I6a3c4e6c22a5ad27bf79916602a004c00e1711e1
    Reviewed-by: Marc Mutz <marc.mutz@qt.io>
    (cherry picked from commit 5e91bb7a8a783777c06d8b8174bfa0b28be13934)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- src/plugins/position/android/src/jnipositioning.cpp
+++ src/plugins/position/android/src/jnipositioning.cpp
@@ -195,8 +195,11 @@ namespace AndroidPositioning {
 
     void unregisterPositionInfoSource(int key)
     {
-        idToPosSource()->remove(key);
-        idToSatSource()->remove(key);
+        if (idToPosSource.exists())
+            idToPosSource->remove(key);
+
+        if (idToSatSource.exists())
+            idToSatSource->remove(key);
     }
 
     enum PositionProvider
